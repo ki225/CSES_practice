67b05d7cbc09d9bf480fc36a91105f8a4f5e9338
ki225 <123147937+ki225@users.noreply.github.com>
2023-02-09 01:27:40 +0800

    Create Building Teams Reference.cpp

---
+++ CSES/Graph Algorithms/Building Teams/Building Teams Reference.cpp
@@ -0,0 +1,78 @@
+#include <iostream>
+#include <vector>
+
+#define endl "\n"
+using namespace std;
+
+int n, m;
+vector< vector<int> > g;
+vector<int> color;
+vector<bool> vis;
+ 
+bool dfs(int u, int c, int par)
+{
+	vis[u] = true;
+	color[u] = c;
+	
+	cout <<"B"<<endl;
+	for(auto v: g[u])
+	{
+		cout <<"start:"<<u<<" size:"<<g[u].size()<<endl;
+		if(v == par) {
+			cout << "*"<<v<<"before"<<par<<endl;
+			continue;
+		}
+		if(color[v] == 0){
+			cout <<"++" << v <<' '<< u <<endl;
+			cout << color[u]<<"  "<<(color[u]^3)<<endl;
+			cout << "C"<<endl;
+			if(!dfs(v, (color[u]^3), u)) 
+				return false;
+		}
+
+			
+        //cout <<v <<' '<<"start:"<<u<<endl;
+		if(color[v] == color[u]) 
+			return false;
+	}
+	return true;
+}
+ 
+bool color_all()
+{
+	for(int i = 1; i <= n; ++i)
+	{
+		if(!vis[i])
+			if(!dfs(i,1,-1)) 
+				return false;
+	}
+	return true;
+}
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cin >> n >> m;
+	g.resize(n+1);
+	color.resize(n+1);
+	vis.resize(n+1);
+ 
+	for(int i = 0; i < m; ++i)
+	{
+		int u, v;
+		cin >> u >> v;
+		g[u].push_back(v);
+		g[v].push_back(u);
+	}
+	bool ans = color_all();
+	if(!ans)
+	{
+		cout << "IMPOSSIBLE" << endl;
+		return 0;
+	}
+	for(int i = 1; i <=n ; ++i)
+	{
+		cout << color[i] << " ";
+	}
+    return 0;
+}
